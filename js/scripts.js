1c39c45487b183e9af26142ff0577e52451caf65
ivan.potehin <ivan.potehin@travelline.ru>
2020-02-28 09:23:28 +0300

    Доработки.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
-var btn_search_formm = document.querySelector('.search-header'),
-    search_form = document.querySelector('.form-content');
+var btn_search_form = document.querySelector('.search-header'),
+    search_form = document.querySelector('.form-content'),
+    dateEntry = document.querySelector('.arrival-control');
 
-search_form.classList.add('modal');
+if(search_form !== null) {
+  search_form.classList.add('modal');
 
-btn_search_formm.addEventListener('click', function(evt){
-  evt.preventDefault();
-  if(search_form.classList.contains('modal-show')) {
-    search_form.classList.remove('modal-show');
-    search_form.classList.remove('modal-error');
+  btn_search_form.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (search_form.classList.contains('modal-show')) {
+      search_form.classList.remove('modal-show');
 
-  } else {
-    search_form.classList.add('modal-show');
-  };
-});
+    } else {
+      search_form.classList.add('modal-show');
+    }
+    ;
+    dateEntry.focus();
+  });
+}
