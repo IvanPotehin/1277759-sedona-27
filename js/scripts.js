6e71023e0a95bc2db9063e83df92d7a0623fd798
ivan.potehin <ivan.potehin@travelline.ru>
2020-03-10 21:29:17 +0300

    Последние правки.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 var btn_search_form = document.querySelector('.search-header'),
     search_form = document.querySelector('.form-content'),
-    dateEntry = document.querySelector('.arrival-control');
+    dateEntry = document.querySelector('.arrival-control'),
+    form_submit = document.querySelector('.search-btn'),
+    arrival = search_form.querySelector('[name=arrival]'),
+    departure = search_form.querySelector('[name=departure]'),
+    adults = search_form.querySelector('[name=adults]'),
+    children = search_form.querySelector('[name=children]');
 
 if(search_form !== null) {
   search_form.classList.add('modal');
@@ -12,8 +17,17 @@ if(search_form !== null) {
 
     } else {
       search_form.classList.add('modal-show');
-    }
-    ;
+    };
     dateEntry.focus();
+  });
+
+  search_form.addEventListener("submit", function (evt) {
+    if (!arrival.value || !departure.value || !adults.value || !children.value) {
+      evt.preventDefault();
+      search_form.classList.add("modal-error");
+    }
+    else {
+      return true;
+    }
   });
 }
